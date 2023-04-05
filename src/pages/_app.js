5f562116e369071853dbae8f961a01597d2dcb6a
japeetifromtahiti <japeetifromtahiti@gmail.com>
2023-04-04 19:30:20 -0700

    Added top navigation bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,42 @@
+```js
 import '@/styles/globals.css'
+import Link from 'next/link'
+
+function TopNavbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/docs">
+            <a>Docs</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/learn">
+            <a>Learn</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/templates">
+            <a>Templates</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/deploy">
+            <a>Deploy</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TopNavbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
+```
