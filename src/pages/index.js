bdcc4f178d6fe6855796bab9ca1f6b4665ccfde2
japeetifromtahiti <japeetifromtahiti@gmail.com>
2023-04-06 23:45:18 -0700

    `Add top navigation bar`

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,30 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/docs">
+              <a>Docs</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/learn">
+              <a>Learn</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/templates">
+              <a>Templates</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/deploy">
+              <a>Deploy</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
